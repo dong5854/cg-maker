7c9b7bd7adba5f7e6c1cc3e7788854dc962845ec
dongyoung <dong5854@gmail.com>
2022-07-07 16:55:44 +0900

    feat: chatService inferface method added

--- src/main/java/com/html/cgmaker/domain/chat/ChatService.java
+++ src/main/java/com/html/cgmaker/domain/chat/ChatService.java
@@ -1,7 +1,14 @@
 package com.html.cgmaker.domain.chat;
 
+import com.html.cgmaker.web.chat.ChatRoomDto;
 import org.springframework.web.socket.WebSocketSession;
 
+import java.util.List;
+
 public interface ChatService {
-        <T> void sendMessage(WebSocketSession session, T message);
+        void init();
+        public List<ChatRoomDto> findAllRoom();
+        public ChatRoomDto findRoomById(String roomId);
+        public ChatRoomDto createRoom(String name);
+        public <T> void sendMessage(WebSocketSession session, T message);
 }
