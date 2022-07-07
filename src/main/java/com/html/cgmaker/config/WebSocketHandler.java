d6e61fb7d70b32511b279e5b24faea8ee2f132e0
dongyoung <dong5854@gmail.com>
2022-07-07 15:36:14 +0900

    feat: websocket config

--- src/main/java/com/html/cgmaker/config/WebSocketHandler.java
+++ src/main/java/com/html/cgmaker/config/WebSocketHandler.java
@@ -1,2 +1,20 @@
-package com.html.cgmaker.config;public class WebSocketHandler {
+package com.html.cgmaker.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Slf4j
+@Component
+public class WebSocketHandler extends TextWebSocketHandler {
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String payload = message.getPayload();
+        log.info("payload {}", payload);
+        TextMessage textMessage = new TextMessage(("Welcome to chatting server"));
+        session.sendMessage(textMessage);
+    }
 }
