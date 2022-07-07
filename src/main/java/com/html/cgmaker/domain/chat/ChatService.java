7559b48352aa413e1f2b9502013811db12b42035
dongyoung <dong5854@gmail.com>
2022-07-07 17:24:12 +0900

    feat: chat service

--- src/main/java/com/html/cgmaker/domain/chat/ChatService.java
+++ src/main/java/com/html/cgmaker/domain/chat/ChatService.java
@@ -1,14 +1,14 @@
 package com.html.cgmaker.domain.chat;
 
-import com.html.cgmaker.web.chat.ChatRoomDto;
+import com.html.cgmaker.web.chat.dto.ChatRoom;
 import org.springframework.web.socket.WebSocketSession;
 
 import java.util.List;
 
 public interface ChatService {
         void init();
-        public List<ChatRoomDto> findAllRoom();
-        public ChatRoomDto findRoomById(String roomId);
-        public ChatRoomDto createRoom(String name);
+        public List<ChatRoom> findAllRoom();
+        public ChatRoom findRoomById(String roomId);
+        public ChatRoom createRoom(String name);
         public <T> void sendMessage(WebSocketSession session, T message);
 }
