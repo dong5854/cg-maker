ff3e2eaa0f5d081be70be97c0ac6b6e9bbd1cbe3
dongyoung <dong5854@gmail.com>
2022-07-18 09:09:30 +0900

    fix: 빼먹은 annotation 추가

--- src/main/java/com/html/cgmaker/web/chat/dto/ChatRoom.java
+++ src/main/java/com/html/cgmaker/web/chat/dto/ChatRoom.java
@@ -2,11 +2,12 @@ package com.html.cgmaker.web.chat.dto;
 
 import com.html.cgmaker.domain.chat.ChatService;
 import lombok.Builder;
+import lombok.Getter;
 import org.springframework.web.socket.WebSocketSession;
 
 import java.util.HashSet;
 import java.util.Set;
-
+@Getter
 public class ChatRoom {
     private String roomId;
     private String name;
